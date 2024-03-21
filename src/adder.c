8c5e79d4efecff46cbc5ba2730042a5d1020d0e0
Adriex <adrielchagay@gmail.com>
2024-03-21 17:16:21 -0300

    CMake de tests corregido para coverage

--- src/adder.c
+++ src/adder.c
@@ -3,3 +3,18 @@
 int sumar(int a, int b) {
     return a + b;
 }
+
+int restar(int a, int b) {
+    return a - b;
+}
+
+int multiplicar(int a, int b) {
+    return a * b;
+}
+
+int dividir(int a, int b) {
+    if (b == 0) {
+        return 0;
+    }
+    return a / b;
+}
--- tests/unit/test_unit_program.c
+++ tests/unit/test_unit_program.c
@@ -8,6 +8,23 @@ void test_sumar(void) {
     TEST_ASSERT_NOT_EQUAL(4, sumar(5, 3));
 }
 
+void test_restar(void) {
+    TEST_ASSERT_EQUAL(2, restar(5, 3));
+    TEST_ASSERT_EQUAL(4, restar(7, 3));
+}
+
+void test_multiplicar(void) {
+    TEST_ASSERT_EQUAL(15, multiplicar(5, 3));
+    TEST_ASSERT_EQUAL(21, multiplicar(7, 3));
+}
+
+void test_dividir(void) {
+    TEST_ASSERT_EQUAL(2, dividir(6, 3));
+    TEST_ASSERT_EQUAL(3, dividir(9, 3));
+    TEST_ASSERT_NOT_EQUAL(3, dividir(6, 3));
+    TEST_ASSERT_NOT_EQUAL(2, dividir(9, 3));
+}
+
 void setUp(void) {
     // set stuff up here
     printf("Setting up\n");
@@ -21,6 +38,8 @@ void tearDown(void) {
 int main() {
     UNITY_BEGIN();
     RUN_TEST(test_sumar);
-    
+    RUN_TEST(test_restar);
+    RUN_TEST(test_multiplicar);
+    RUN_TEST(test_dividir);
     return UNITY_END();
 }
